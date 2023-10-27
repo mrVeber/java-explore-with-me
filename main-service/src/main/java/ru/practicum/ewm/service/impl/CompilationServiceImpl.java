8c750ff3249f22e1bba4bddb568c5756b308420e
Veber <pavelbo92@yandex.ru>
2023-10-27 10:49:31 +0300

    fix: main_svc: Внесены изменения по комментариям ревьювера CompilationServiceImpl.java 59стр, добавлена проверка на null

--- main-service/src/main/java/ru/practicum/ewm/service/impl/CompilationServiceImpl.java
+++ main-service/src/main/java/ru/practicum/ewm/service/impl/CompilationServiceImpl.java
@@ -57,7 +57,7 @@ public class CompilationServiceImpl implements CompilationService {
 
         compilation.setPinned(Optional.ofNullable(update.getPinned()).orElse(compilation.getPinned()));
         if (compilation.getTitle() == null) {
-                throw new UncorrectedParametersException("null");
+                throw new UncorrectedParametersException("Title не может состоять из пробелов");
         }
         compilation.setTitle(Optional.ofNullable(update.getTitle()).orElse(compilation.getTitle()));
         return CompilationMapper.toDto(compilation);
