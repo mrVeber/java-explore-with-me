c24b4e7ca5804931dcbf6a945aad064676fb7ce3
Veber <pavelbo92@yandex.ru>
2023-10-30 11:22:42 +0300

    fix: feature_comments: Внесены изменения по ТЗ Дипломного проекта

--- main-service/src/main/java/ru/practicum/ewm/repository/EventRepository.java
+++ main-service/src/main/java/ru/practicum/ewm/repository/EventRepository.java
@@ -2,7 +2,6 @@ package ru.practicum.ewm.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import ru.practicum.ewm.model.Category;
 import ru.practicum.ewm.model.Event;
 
 import java.util.List;
